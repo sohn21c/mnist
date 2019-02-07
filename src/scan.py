1fca621ff983e7bb22ea2d7f1e6db55636e47f10
James S <sohn21c@hotmail.com>
2019-02-07 14:32:11 -0600

    initial commit on scan.py, performing edge detection, finding contour and transforming the given area to bird-eye view

---
+++ src/scan.py
@@ -0,0 +1,92 @@
+## Code based on Pyimaagesearch tutorial
+## Source: https://www.pyimagesearch.com/
+
+
+from transform import four_point_transform, order_points
+from skimage.filters import threshold_local
+import numpy as np 
+import argparse
+import cv2
+import imutils
+
+### Edge detection
+
+# argpars structure
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--image", required = True, help = "Path to the image to be scanned")
+args = vars(ap.parse_args())
+
+# load the image and compute the ratio of the old and new heights, clone it and resize it
+image = cv2.imread(args["image"])
+print ("Image pixel size", image.shape)
+ratio = image.shape[0] / 500.0
+orig = image.copy()
+image = imutils.resize(image, height = 500)
+
+# convert the image to grayscale, blur it and find edges in the image
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+gray = cv2.GaussianBlur(gray, (5,5), 0)
+edged = cv2.Canny(gray, 75, 200)
+
+# show the original image and the edge detected image
+print ("Step 1: Edge detection")
+
+cv2.imshow("Image", image)
+cv2.imshow("Edged", edged)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+
+
+### Find the Contour
+
+# find the contours in the edged image, keeping only the largest ones, and initialize the screen contour
+cnts = cv2.findContours(edged.copy(), cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+cnts = imutils.grab_contours(cnts)
+cnts = sorted(cnts, key = cv2.contourArea, reverse = True)[:5]
+
+# loop opver the contours
+for c in cnts:
+	# approximate the contour
+	peri = cv2.arcLength(c, True)
+	approx = cv2.approxPolyDP(c, 0.02 * peri, True)
+
+	# if our approximated contour has four points, then we can assume that we have found our screen
+	if len(approx) == 4:
+		screenCnt = approx
+		break
+
+# show the contour (outline) of the piece of paper
+print ("Step 2: Find contours of paper")
+
+cv2.drawContours(image, [screenCnt], -1, (0, 255, 0), 2)
+cv2.imshow("Outline", image)
+
+
+
+
+### Transform to bird-eye view
+
+# apply the four point transform to obtain a top-down view of the original image
+warped = four_point_transform(orig, screenCnt.reshape(4,2) * ratio)
+
+# convert the warped image to grayscale, then threshold it to give it that 'black and white' paper effect
+warped = cv2.cvtColor(warped, cv2.COLOR_BGR2GRAY)
+resizeWarped = imutils.resize(warped, height = 28)
+print ("Pixel size of resized scan", resizeWarped.shape)
+T = threshold_local(warped, 201, offset = 10, method = "gaussian")
+T2 = threshold_local(resizeWarped, 9, offset = 10, method = "gaussian")
+warped = (warped > T).astype("uint8") * 255
+resizeWarped = (resizeWarped > T2).astype("uint8") * 255
+
+
+# show the original and scanned image
+print ("Step 3: Apply perspective transform")
+# cv2.imshow("Original", imutils.resize(orig, height = 650))
+cv2.imshow("Scanned", imutils.resize(warped, height = 650))
+cv2.imshow("Resized", imutils.resize(resizeWarped, height = 650))
+cv2.imwrite("saved1.jpg", imutils.resize(warped, height = 650))
+print ("img saved")
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
